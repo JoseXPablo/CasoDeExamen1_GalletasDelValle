1bb761b99c9d7817a80aa8aeca3caf85b9bd1892
jpali <jpali@DESKTOP-3EIL9D0>
2025-10-24 16:30:21 -0600

    Primer commit - estructura base del proyecto

---
+++ src/main/java/com/mycompany/casodeexamen1_galletasdelvalle/git/CasoDeExamen1_GalletasDelValleGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casodeexamen1_galletasdelvalle.git;
+
+/**
+ *
+ * @author jpali
+ */
+public class CasoDeExamen1_GalletasDelValleGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
