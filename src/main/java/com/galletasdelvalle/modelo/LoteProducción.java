c6fffa46489f6c0104df502c66995c62542f9261
jpali <jpali@DESKTOP-3EIL9D0>
2025-10-26 16:02:01 -0600

    Reconecto proyecto completo

---
+++ src/main/java/com/galletasdelvalle/modelo/LoteProducción.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.galletasdelvalle.modelo;
+
+/**
+ *
+ * @author jpali
+ */
+public abstract class LoteProducción {
+    // Atributos (privados)
+    private String idLote;
+    private String tipoGalleta;        // "Avena", "Chocolate", etc.
+    private String fechaFabricacion;   
+    private int unidadesProducidas;
+    private double horasFabricacion;
+
+    // Constructor
+    public LoteProducción(String idLote, String tipoGalleta, String fechaFabricacion,
+                          int unidadesProducidas, double horasFabricacion) throws Exception {
+        setIdLote(idLote);
+        setTipoGalleta(tipoGalleta);
+        setFechaFabricacion(fechaFabricacion);
+        setUnidadesProducidas(unidadesProducidas);
+        setHorasFabricacion(horasFabricacion);
+    }
+
+    // Getters y Setters
+    public String getIdLote() { return idLote; }
+    public void setIdLote(String idLote) throws Exception {
+        if (idLote == null || idLote.equals("")) {
+            throw new Exception("ID inválido");
+        }
+        this.idLote = idLote;
+    }
+
+    public String getTipoGalleta() { return tipoGalleta; }
+    public void setTipoGalleta(String tipoGalleta) throws Exception {
+        if (tipoGalleta == null || tipoGalleta.equals("")) {
+            throw new Exception("Tipo inválido");
+        }
+        this.tipoGalleta = tipoGalleta;
+    }
+
+    public String getFechaFabricacion() { return fechaFabricacion; }
+    public void setFechaFabricacion(String fechaFabricacion) throws Exception {
+        if (fechaFabricacion == null || fechaFabricacion.equals("")) {
+            throw new Exception("Fecha inválida");
+        }
+        this.fechaFabricacion = fechaFabricacion;
+    }
+
+    public int getUnidadesProducidas() { return unidadesProducidas; }
+    public void setUnidadesProducidas(int unidadesProducidas) throws Exception {
+        if (unidadesProducidas <= 0) {
+            throw new Exception("Unidades deben ser > 0");
+        }
+        this.unidadesProducidas = unidadesProducidas;
+    }
+
+    public double getHorasFabricacion() { return horasFabricacion; }
+    public void setHorasFabricacion(double horasFabricacion) throws Exception {
+        if (horasFabricacion <= 0) {
+            throw new Exception("Horas deben ser > 0");
+        }
+        this.horasFabricacion = horasFabricacion;
+    }
+
+    // Método abstracto (polimorfismo): cada subclase define su costo
+    public abstract double calcularCostoProduccion();
+
+    // toString sencillo (para imprimir)
+    @Override
+    public String toString() {
+        return idLote + " - " + tipoGalleta + " - " + fechaFabricacion +
+               " | unidades=" + unidadesProducidas + " | horas=" + horasFabricacion;
+    }
+}
--- src/main/java/com/mycompany/casodeexamen1_galletasdelvalle/git/CasoDeExamen1_GalletasDelValleGit.java
+++ src/main/java/com/mycompany/casodeexamen1_galletasdelvalle/git/CasoDeExamen1_GalletasDelValleGit.java
@@ -5,12 +5,25 @@
 package com.mycompany.casodeexamen1_galletasdelvalle.git;
 
 /**
- *
+ *Universidad Fidélitas
+ * 
+ * Grupo 5
+ * 
+ * Programación Cliente / Servidor
+ * 
+ * Caso Numero 1
+ * 
  * @author jpali
  */
 public class CasoDeExamen1_GalletasDelValleGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+        
+        
+        
+        
+        
     }
 }
